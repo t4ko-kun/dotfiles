d53d7b9f7c66df3fdb7165d4fa068669edad223b
Manuel Breiden <manuel.breiden@stud.uni-hannover.de>
2022-11-23 13:18:40 +0100

    wezterm: Hide tab bar when only one tab exists

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,9 +2,11 @@ local wezterm = require 'wezterm';
 
 return {
     window_close_confirmation = "NeverPrompt",
-    font = wezterm.font("Hack"),
+    hide_tab_bar_if_only_one_tab = true,
     keys = {
-        {key="\"", mods="CMD", action=wezterm.action{SplitHorizontal={args={"zsh"}}}
-        },
-    }
+        {key="|", mods="CMD", action=wezterm.action{SplitHorizontal={args={"zsh"}}}},
+        {key="_", mods="CMD", action=wezterm.action{SplitVertical={args={"zsh"}}}},
+    },
+    font = wezterm.font("Hack"),
+    color_scheme = "sol"
 }
